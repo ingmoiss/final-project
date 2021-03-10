3884283eec12f8d93c2e1c8cb3cb16151c9f09b5
davidmorasalazar <davidmsalazar0299@gmail.com>
2021-03-10 15:34:29 +0000

    my final commit v3

--- src/front/js/component/navbarcentral.js
+++ src/front/js/component/navbarcentral.js
@@ -13,22 +13,8 @@ export const Navbarcentral = () => {
 					</a>
 				</Link>
 				&nbsp;
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarNav"
-					aria-controls="navbarNav"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon" />
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
+				<div id="navbarNav">
 					<ul className="navbar-nav">
-						<li className="nav-item active" />
-						<li className="nav-item">
-							<a className="nav-link" href="#" />
-						</li>
 						<li className="nav-item">
 							<button id="iniciar" type="button" className="btn btn-primary">
 								Iniciar Sesion &nbsp;

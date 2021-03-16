073a12d0bfc19239c818f6d0a06c338c08ee35ea
LuisAlvarado-19 <luafer95@gmail.com>
2021-03-16 20:07:09 +0000

    nada

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,8 @@ import { Context } from "../store/appContext";
 import "../../styles/home.scss";
 import { Carousel, Container, NavItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import logo from "../../img/Luis-Larraburo.jpg";
+import logo1 from "../../img/Luis-Alvarado1.jpg";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
@@ -12,15 +14,12 @@ export const Home = () => {
 				<Carousel.Item rounded>
 					<a href="https://www.pedigree.com/" target="_blank" rel="noopener noreferrer">
 						<img
+							style={{ height: "500px" }}
 							className="d-block w-100"
 							src="https://www.elpublicista.info/wp-content/uploads/2018/06/Pedrigree.png"
 							alt="Characters"
 							rounded
 						/>
-						<Carousel.Caption>
-							<h3>Characters</h3>
-							<p>Find Your Favorites Star Wars Characters</p>
-						</Carousel.Caption>
 					</a>
 				</Carousel.Item>
 
@@ -65,39 +64,78 @@ export const Home = () => {
 					</div>
 				</div>
 			</div>
-			<Carousel className="carousel1">
+			<Carousel className="carousel1" style={{ marginTop: "50px" }}>
 				<Carousel.Item rounded>
-					<img
-						className="d-block w-100"
-						src="https://3.bp.blogspot.com/--S8RvjDIcsY/UcJXV81oxzI/AAAAAAAAE_Y/-mYFSsRCjxY/s1600/Descargar+Pack+Fondos+de+Pantalla+Animales+en+HD+(396).jpg"
-						alt="Characters"
-						rounded
-					/>
+					<div className="media">
+						<div className="media-body">
+							<h5 className="mt-0 mb-1">Media object</h5>
+							Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+							Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+							ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+						</div>
+						<img
+							className="d-flex ml-3"
+							src="/images/pathToYourImage.png"
+							alt="Generic placeholder image"
+						/>
+					</div>
 				</Carousel.Item>
 
 				<Carousel.Item>
-					<img
-						className="d-block w-100"
-						src="https://www.mundoperros.es/wp-content/uploads/2017/07/perro-adoptado-830x553.jpg"
-						alt="Characters"
-						rounded
-					/>
+					<div className="media">
+						<div className="media-body">
+							<h5 className="mt-0 mb-1">Media object</h5>
+							Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+							Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+							ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+						</div>
+						<img
+							className="d-flex ml-3"
+							src="/images/pathToYourImage.png"
+							alt="Generic placeholder image"
+						/>
+					</div>
 				</Carousel.Item>
 				<Carousel.Item>
-					<img
-						className="d-block w-100"
-						src="https://www.24horas.cl/incoming/article1112091.ece/ALTERNATES/w620h350/dozer.jpg"
-						alt="Characters"
-						rounded
-					/>
+					<div className="media">
+						<div id="texto" className="media-body">
+							<h5 className="mt-0 mb-1">Mas que una mascota!</h5>
+							<a id="texto2">
+								A sido una de la expericias mas bonitas que he tenido en mi vida adopte a Scott cuando
+								apenas tenia quince dias de nacido, hubieron momentos en los que pense que realmente no
+								lo iba lograrlo puesto que el estaba muy pequeño pero aqui esta y tiene nueve años
+								conmigo y espero que este muchos años mas, y por supuesto tambien tengo a mi perrita
+								negra que justo va cumplir un año de vivir y la concentida de la casa :) adoptar una
+								mascota es un acto muy noble y animalito estara muy agradecido asi que los invito
+								adoptar a ustedes tambien!!
+							</a>
+						</div>
+						<div data-spy="scroll" id="texto2" data-target="texto" data-offset="0" className="" />
+						<img
+							className="d-block w-50"
+							// style={{ borderRadius: "50%" }}
+							src={logo1}
+							alt="Generic placeholder image"
+						/>
+					</div>
 				</Carousel.Item>
 				<Carousel.Item>
-					<img
-						className="d-block w-100"
-						src="https://media.cnnchile.com/sites/4/2020/12/20090.jpg"
-						alt="Characters"
-						rounded
-					/>
+					<div className="media">
+						<div className="media-body">
+							<h5 className="mt-0 mb-1">Una amiga fiel en casa!</h5>
+							Así es como puedo describir mis días desde la llegada de Coqueta a mi casa, es una perrita
+							muy bien portada, amigable y fiel. Coqueta poco a poco ha ido superando su abandono en las
+							calles, desde que la adoptamos ha estado súper contenta y agradecida. No se cansa de correr
+							por toda la casa, y pues claro, una que otra diablura que sin duda alguna nos saca una
+							sonrisa. Agradezco a Zaguatec por su ayuda en este proceso de adopcion. Recomendado al 100%.
+						</div>
+						<img
+							className="d-block w-50"
+							// style={{ borderRadius: "50%" }}
+							src={logo}
+							alt="Generic placeholder image"
+						/>
+					</div>
 				</Carousel.Item>
 			</Carousel>
 		</Container>

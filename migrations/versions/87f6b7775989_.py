ef8cbcd1522ec35eaff9ccd87f18f3ef2ed960a7
davidmorasalazar <davidmsalazar0299@gmail.com>
2021-03-16 21:33:50 +0000

    final martes

--- migrations/versions/87f6b7775989_.py
+++ migrations/versions/87f6b7775989_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9044e97fad6a
+Revision ID: 87f6b7775989
 Revises: 
-Create Date: 2021-03-16 15:42:50.365413
+Create Date: 2021-03-16 20:45:30.349138
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9044e97fad6a'
+revision = '87f6b7775989'
 down_revision = None
 branch_labels = None
 depends_on = None
